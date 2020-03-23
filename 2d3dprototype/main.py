02337117ae86d7de7bf07fbafdd4a7cfba9e5c4b
Nils Gehlin <nils.gehlin@gmail.com>
2020-03-23 17:53:37 +0100

    Adding .gitignore

--- 2d3dprototype/main.py
+++ 2d3dprototype/main.py
@@ -1,14 +1,15 @@
 from render3d import render3d
-from dicom_viewer import dicom_viewer
-from multiprocessing import Process, Queue, Event
+# from dicom_viewer import dicom_viewer
+# from multiprocessing import Process, Queue, Event
+#
+# directory = r'stanford-ct-new'
+# 
+# mouse_move_event = Event()
+# mouse_queue = Queue()
+# p1 = Process(target=dicom_viewer, args=(directory, mouse_queue, mouse_move_event))
+# p2 = Process(target=render3d, args=(directory, mouse_queue, mouse_move_event))
+# p1.start()
+# p2.start()
+# p1.join()
+# p2.join()
 
-directory = r'stanford-ct-new'
-
-mouse_move_event = Event()
-mouse_queue = Queue()
-p1 = Process(target=dicom_viewer, args=(directory, mouse_queue, mouse_move_event))
-p2 = Process(target=render3d, args=(directory, mouse_queue, mouse_move_event))
-p1.start()
-p2.start()
-p1.join()
-p2.join()
